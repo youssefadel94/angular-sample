df5d4245eaa5553e9f370d9b4464c6656bb94604
youssefadel94 <darkmetalxdragon@hotmail.com>
2016-07-11 19:08:01 +0200

    add books

--- js/controllers/MainControllers.js
+++ js/controllers/MainControllers.js
@@ -1,3 +1,9 @@
 app.controller('MainController', ['$scope', function($scope) {
     $scope.title = 'Top Sellers in Books';
+    $scope.promo = 'best books you will find';
+    $scope.product = {
+        name: 'The Book of Trees',
+        price: 19,
+        pubdate: new Date('2014', '03', '08')
+    };
 }]);
